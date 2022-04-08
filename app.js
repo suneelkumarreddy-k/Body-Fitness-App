5b2a4a008ef2d75d4844dd8b4e3e51efdc08a25d
suneelkumarreddy-k <67097067+suneelkumarreddy-k@users.noreply.github.com>
2022-04-08 10:51:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ try {
 		if (typeof(Storage) !== "undefined") {
 			localStorage.rpm = "true";
 			if (localStorage.length > 1) {
-
+if (confirm("Daily Routine?") {
 				//alert(localStorage.getItem("rpm"));
 				const fdte = new Date();
 				let day = fdte;
@@ -64,6 +64,7 @@ try {
 				localStorage.rpm = "false";
 			}
 		}
+}
 	}
 
 	function ldPgm() {
